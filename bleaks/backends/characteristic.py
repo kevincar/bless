395b85eb516848f9b3cce3ef3a2fa855a7a41bde
Kevin Davis <kevincarrolldavis@gmail.com>
2020-10-14 10:10:12 -0400

    Prefer Flags over Enum
    This change allows us to use bitwise operations on these flags

---
+++ bleaks/backends/characteristic.py
@@ -0,0 +1,14 @@
+from enum import Flag
+
+
+class GattCharacteristicsFlags(Flag):
+    broadcast = 0x0001
+    read = 0x0002
+    write_without_response = 0x0004
+    write = 0x0008
+    notify = 0x0010
+    indicate = 0x0020
+    authenticated_signed_writes = 0x0040
+    extended_properties = 0x0080
+    reliable_write = 0x0100
+    writable_auxiliaries = 0x0200
