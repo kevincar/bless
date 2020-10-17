2d2f3482a1b16a8767c4b825b64803c8726a3d60
Kevin Davis <kevincarrolldavis@gmail.com>
2020-10-16 23:38:56 -0400

    Expose the underlying Server for MacOS

--- bleaks/__init__.py
+++ bleaks/__init__.py
@@ -1,3 +1,9 @@
+import sys
+
+if sys.platform == 'darwin':
+    from bleaks.backends.corebluetooth.server import BleakServerCoreBluetooth as BleakServer  # noqa: F401 E501
+
+
 def check_test() -> bool:
     """
     Verify that testing is functional
