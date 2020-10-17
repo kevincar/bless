b2b4f0bc5148880da10a7c41785ce6edbe83a503
Kevin Davis <kevincarrolldavis@gmail.com>
2020-10-16 23:38:06 -0400

    Add GATTAttributePermissions

--- bleaks/backends/characteristic.py
+++ bleaks/backends/characteristic.py
@@ -12,3 +12,10 @@ class GattCharacteristicsFlags(Flag):
     extended_properties = 0x0080
     reliable_write = 0x0100
     writable_auxiliaries = 0x0200
+
+
+class GATTAttributePermissions(Flag):
+    readable = 0x1
+    writeable = 0x2
+    read_encryption_required = 0x4
+    write_encryption_required = 0x8
