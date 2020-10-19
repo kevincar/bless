4e814f7959b2c2b08f19d5cfe9e6393ae845d28f
Kevin Davis <kevincarrolldavis@gmail.com>
2020-10-19 08:10:46 -0400

    Adding more characteristic exports

--- bleaks/__init__.py
+++ bleaks/__init__.py
@@ -1,7 +1,19 @@
 import sys
 
 if sys.platform == 'darwin':
+    # Server
     from bleaks.backends.corebluetooth.server import BleakServerCoreBluetooth as BleakServer  # noqa: F401 E501
+
+    # Characteristic Classes
+    from bleaks.backends.corebluetooth.characteristic import (  # noqa: F401
+            BleaksGATTCharacteristicCoreBluetooth
+            as BleaksGATTCharacteristic
+            )
+
+from bleaks.backends.characteristic import (  # noqa: E402 F401
+        GattCharacteristicsFlags,
+        GATTAttributePermissions
+        )
 
 
 def check_test() -> bool:
