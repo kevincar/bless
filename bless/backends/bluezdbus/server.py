307b74ab4543250ebb87b979189940cef93a2b52
kevincar <kevincarrolldavis@gmail.com>
2020-12-08 00:11:49 -0500

    Initiail implementation

---
+++ bless/backends/bluezdbus/server.py
@@ -0,0 +1,19 @@
+import asyncio
+
+from asyncio import AbstractEventLoop
+from bless.backends.server import BaseBlessServer
+
+
+class BlessServerBlueZDBus(BaseBlessServer):
+    """
+    The BlueZ DBus implementation of the Bless Server
+
+    Attributes
+    ----------
+    name : str
+        The name of the server that will be advertised]
+
+    """
+
+    def __init__(self, name: str, loop: AbstractEventLoop = None, **kwargs):
+        super(BlessServerBlueZDBus, self).__init__(loop=loop, **kwargs)
