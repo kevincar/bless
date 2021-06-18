878bb447f001b49e9eb918f499f8df456e9c4ef5
Kevin Davis <kevincarrolldavis@gmail.com>
2021-06-17 21:08:36 -0400

    Version bump to 0.2.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="bless",
-    version="0.2.0",
+    version="0.2.1",
     author="Kevin Davis",
     author_email="kevincarrolldavis@gmail.com",
     description="A Bluetooth Low Energy Server supplement to Bleak",
