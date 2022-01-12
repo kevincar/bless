5b28ef41aa323f581dcf934af57bff9b4a891e91
kevincar <kevincarrolldavis@gmail.com>
2022-01-12 12:14:59 -0500

    convert utilities to use dbus-next
    First implementation of obtaining DBus objects with dbus-next

--- bless/backends/bluezdbus/dbus/utils.py
+++ bless/backends/bluezdbus/dbus/utils.py
@@ -1,44 +1,63 @@
-import asyncio
-
 import bleak.backends.bluezdbus.defs as defs  # type: ignore
 
-from typing import Dict, Optional
+from typing import Dict, Optional, cast
 
-from txdbus import client  # type: ignore
-from txdbus.objects import RemoteDBusObject  # type: ignore
+from dbus_next.aio import MessageBus, ProxyObject, ProxyInterface
+from dbus_next.introspection import Node
 
 
-async def find_adapter(bus: client, loop: asyncio.AbstractEventLoop) -> Optional[str]:
+async def find_adapter(bus: MessageBus) -> Optional[str]:
     """
-    Returns the first object that the bluez service has that has a GattManager1
+    Returns the first object within the bluez service that has a GattManager1
     interface
+
+    Parameters
+    ----------
+    bus : MessageBus
+        The currently connected message bus to communicate with BlueZ
+
+    Returns
+    -------
+    Optional[str]
+        The name of the adapter interface on the dbus.
+        None if it does not find an adapter
     """
-    bluez_obj: RemoteDBusObject = await bus.getRemoteObject(
-        defs.BLUEZ_SERVICE, "/"
-    ).asFuture(loop)
+    bluez_node: Node = await bus.introspect(defs.BLUEZ_SERVICE, "/")
+    bluez_obj: ProxyObject = bus.get_proxy_object(defs.BLUEZ_SERVICE, "/", bluez_node)
+    interface: ProxyInterface = bluez_obj.get_interface(
+        defs.OBJECT_MANAGER_INTERFACE
+    )
+    bt_objects: Dict = await interface.call_get_managed_objects()  # type: ignore
 
-    om_objects: Dict = await bluez_obj.callRemote(
-        "GetManagedObjects", interface=defs.OBJECT_MANAGER_INTERFACE
-    ).asFuture(loop)
-
-    for o, props in om_objects.items():
+    for objs, props in bt_objects.items():
         if defs.GATT_MANAGER_INTERFACE in props.keys():
-            return o
+            return objs
 
     return None
 
 
-async def get_adapter(bus: client, loop: asyncio.AbstractEventLoop) -> RemoteDBusObject:
+async def get_adapter(bus: MessageBus) -> Optional[ProxyObject]:
     """
     Gets the bluetooth adapter
 
+    Parameters
+    ----------
+    bus : MessageBus
+        The connected DBus object
+
     Returns
     -------
-    DBusObject
+    Optional[ProxyObject]
         The adapter object
+        None if not found
     """
-    adapter_path: Optional[str] = await find_adapter(bus, loop)
-    adapter_obj: RemoteDBusObject = await bus.getRemoteObject(
-        defs.BLUEZ_SERVICE, adapter_path
-    ).asFuture(loop)
+    oadapter_path: Optional[str] = await find_adapter(bus)
+    if oadapter_path is None:
+        return None
+
+    adapter_path: str = cast(str, oadapter_path)
+    adapter_node: Node = await bus.introspect(defs.BLUEZ_SERVICE, adapter_path)
+    adapter_obj: ProxyObject = bus.get_proxy_object(
+        defs.BLUEZ_SERVICE, adapter_path, adapter_node
+    )
     return adapter_obj
