ead13991532a7202238f9b960715f54c647d541d
kevincar <kevincarrolldavis@gmail.com>
2022-01-12 12:16:58 -0500

    dbus-next advertisement object
    A potential problem with dbus-next object implementation is that there
    seemse to be a requirement that object members are static.
    Advertisement objects may or may not need all of its members. For
    example, is solicit UUIDs is empty, the BlueZ backend requires that it
    not be present at all

--- bless/backends/bluezdbus/dbus/advertisement.py
+++ bless/backends/bluezdbus/dbus/advertisement.py
@@ -2,8 +2,7 @@ from enum import Enum
 
 from typing import List, Dict, TYPE_CHECKING
 
-from txdbus.objects import DBusObject, DBusProperty, dbusMethod  # type: ignore
-from txdbus.interface import DBusInterface, Method, Property  # type: ignore
+from dbus_next.service import ServiceInterface, method, dbus_property
 
 if TYPE_CHECKING:
     from bless.backends.bluezdbus.dbus.application import (  # type: ignore
@@ -16,32 +15,12 @@ class Type(Enum):
     PERIPHERAL = "peripheral"
 
 
-class BlueZLEAdvertisement(DBusObject):
+class BlueZLEAdvertisement(ServiceInterface):
     """
     org.bluez.LEAdvertisement1 interface implementation
     """
 
     interface_name: str = "org.bluez.LEAdvertisement1"
-
-    iface: DBusInterface = DBusInterface(
-        interface_name,
-        Method("Release"),
-        Property("Type", "s"),
-        Property("ServiceUUIDs", "as"),
-        Property("ManufacturerData", "a{qay}"),
-        Property("SolicitUUIDs", "as"),
-        Property("ServiceData", "a{sv}"),
-        Property("IncludeTxPower", "b"),
-    )
-
-    dbusInterfaces: List[DBusInterface] = [iface]
-
-    ad_type: DBusProperty = DBusProperty("Type")
-    service_uuids: DBusProperty = DBusProperty("ServiceUUIDs")
-    # manufacturer_data = DBusProperty("ManufacturerData")
-    # solicit_uuids = DBusProperty("SolicitUUIDs")
-    # service_data = DBusProperty("ServiceData")
-    include_tx_power: DBusProperty = DBusProperty("IncludeTxPower")
 
     def __init__(
         self,
@@ -61,19 +40,67 @@ class BlueZLEAdvertisement(DBusObject):
         app : BlueZGattApplication
             The Application that is responsible for this advertisement
         """
-        self.ad_type: str = advertising_type.value
         self.path = app.base_path + "/advertisement" + str(index)
 
-        self.service_uuids: List[str] = []
-        self.manufacturer_data: Dict = {}
-        self.solicit_uuids = [""]
-        self.service_data = {"": 0}
+        self._type: str = advertising_type.value
+        self._service_uuids: List[str] = []
+        self._manufacturer_data: Dict = {}
+        self._solicit_uuids: List[str] = [""]
+        self._service_data: Dict = {}
 
-        self.include_tx_power: bool = False
+        self._include_tx_power: bool = False
 
         self.data = None
-        super(BlueZLEAdvertisement, self).__init__(self.path)
+        super(BlueZLEAdvertisement, self).__init__(self.interface_name)
 
-    @dbusMethod(interface_name, "Release")
+    @method()
     def Release(self):  # noqa: N802
         print("%s: Released!" % self.path)
+
+    @dbus_property()
+    def Type(self) -> "s":  # type: ignore # noqa: F821
+        return self._type
+
+    @Type.setter  # type: ignore
+    def Type(self, type: "s"):  # type: ignore # noqa: F821 F722
+        self._type = type
+
+    @dbus_property()
+    def ServiceUUIDs(self) -> "as":  # type: ignore # noqa: F821 F722
+        return self._service_uuids
+
+    @ServiceUUIDs.setter  # type: ignore
+    def ServiceUUIDs(self, service_uuids: "as"):  # type: ignore # noqa: F821 F722
+        self._service_uuids = service_uuids
+
+    @dbus_property()
+    def ManufacturerData(self) -> "a{qv}":  # type: ignore # noqa: F821 F722
+        return self._manufacturer_data
+
+    @ManufacturerData.setter  # type: ignore
+    def ManufacturerData(self, data: "a{qv}"):  # type: ignore # noqa: F821 F722
+        self._manufacturer_data = data
+
+    # @dbus_property()
+    # def SolicitUUIDs(self) -> "as":  # type: ignore # noqa: F821 F722
+        # return self._solicit_uuids
+
+    # @SolicitUUIDs.setter  # type: ignore
+    # def SolicitUUIDs(self, uuids: "as"):  # type: ignore # noqa: F821 F722
+        # self._solicit_uuids = uuids
+
+    @dbus_property()
+    def ServiceData(self) -> "a{sv}":  # type: ignore # noqa: F821 F722
+        return self._service_data
+
+    @ServiceData.setter  # type: ignore
+    def ServiceData(self, data: "a{sv}"):  # type: ignore # noqa: F821 F722
+        self._service_data = data
+
+    @dbus_property()
+    def IncludeTxPower(self) -> "b":  # type: ignore # noqa: F821
+        return self._include_tx_power
+
+    @IncludeTxPower.setter  # type: ignore
+    def IncludeTxPower(self, include: "b"):  # type: ignore # noqa: F821
+        self._include_tx_power = include
